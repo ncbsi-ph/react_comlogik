a7ad8c90f491eb903911c7622dd55cb77f11b315
Revo Jet Socorro <45466103+cysmix12@users.noreply.github.com>
2020-04-06 12:24:32 +0800

    Update Section component to support className

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Section = props => {
-  let removePadding = '';
-  if (props.removePadding !== null && props.removePadding !== '') {
-    removePadding = `uk-padding-remove-${props.removePadding}`;
+const Section = (props) => {
+  let classes = props.className;
+  if (props.removePadding !== '') {
+    classes = classes
+      .concat(' ')
+      .concat(`uk-padding-remove-${props.removePadding}`);
   }
+  classes = classes.trim();
 
   if (!props.container) {
     return (
-      <div className={`uk-section ${removePadding}`} style={props.style}>
+      <div className={`uk-section ${classes}`} style={props.style}>
         {props.children}
       </div>
     );
   }
 
   return (
-    <div className={`uk-section ${removePadding}`} style={props.style}>
+    <div className={`uk-section ${classes}`} style={props.style}>
       <div className="uk-container">{props.children}</div>
     </div>
   );
 };
 
-const Grid = props => {
+const Grid = (props) => {
   let classes = props.className;
   if (props.childWidth !== '') {
     const childWidth = props.childWidth.split(' ');
     if (childWidth.length) {
-      childWidth.forEach(width => {
+      childWidth.forEach((width) => {
         classes = classes.concat(' ').concat(`uk-child-width-${width}`);
       });
     }
@@ -44,12 +47,12 @@ const Grid = props => {
   );
 };
 
-const Column = props => {
+const Column = (props) => {
   let classes = props.className;
   if (props.width !== '') {
     const width = props.width.split(' ');
     if (width.length) {
-      width.forEach(_width => {
+      width.forEach((_width) => {
         classes = classes.concat(' ').concat(`uk-width-${_width}`);
       });
     }
@@ -66,43 +69,45 @@ const Column = props => {
 // Property types
 
 Section.propTypes = {
+  className: PropTypes.string,
   removePadding: PropTypes.string,
   style: PropTypes.object,
-  container: PropTypes.bool
+  container: PropTypes.bool,
 };
 
 Grid.propTypes = {
   className: PropTypes.string,
   childWidth: PropTypes.string,
   masonry: PropTypes.bool,
-  parallax: PropTypes.number
+  parallax: PropTypes.number,
 };
 
 Column.propTypes = {
   className: PropTypes.string,
   style: PropTypes.object,
-  width: PropTypes.string
+  width: PropTypes.string,
 };
 
 // Default values for properties
 
 Section.defaultProps = {
+  className: '',
   removePadding: '',
   style: {},
-  container: true
+  container: true,
 };
 
 Grid.defaultProps = {
   className: '',
   childWidth: '',
   masonry: false,
-  parallax: 0
+  parallax: 0,
 };
 
 Column.defaultProps = {
   className: '',
   style: {},
-  width: ''
+  width: '',
 };
 
 export { Section, Grid, Column };
