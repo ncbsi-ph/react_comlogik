c721d668c4c517a3fe4cc71521cb8f0eb1e340df
Ivy Gozarin <38131621+itsbunniie@users.noreply.github.com>
2020-04-06 13:07:29 +0800

    landing

--- src/components/Index/Landing.js
+++ src/components/Index/Landing.js
@@ -10,7 +10,7 @@ const Landing = () => {
           <Grid childWidth="1-1">
             <Column>
             <img className="logo-bg" src="static/logo-bg.svg" />
-            <img className="landing-bg-1" src="static/landing-bg-1.svg" />
+            <img className="landing-bg" src="static/landing-bg.svg" />
               <h1>Beyond Just Information</h1>
             </Column>
             <Column>
@@ -22,7 +22,7 @@ const Landing = () => {
             <Column>
               <Grid childWidth="auto" className="uk-grid-small">
                 <Column>
-                  <button className="uk-button secondary-button uk-margin-right">
+                  <button className="uk-button secondary-button uk-margin-remove-right">
                     Request a demo
                   </button>
                 </Column>
