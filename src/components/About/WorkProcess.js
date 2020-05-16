6a77fdb18f66d97831a5303d7c76b7f51d8c29eb
Ivy Gozarin <38131621+itsbunniie@users.noreply.github.com>
2020-05-16 15:47:18 +0800

    animation (index, about), contact links

--- src/components/About/WorkProcess.js
+++ src/components/About/WorkProcess.js
@@ -26,36 +26,44 @@ const WorkProcess = () => {
               <h1>Our work process</h1>
             </Column>
             <Column width="1-2@s 1-1@l">
-              <Process
-                image="static/process-1.svg"
-                process="Project Planning"
-                description="All the components needed to set up the project success
-                        will be described. It's the process you go through to
-                        establish the steps required to define our project
-                        objectives, clarify the scope of what needs to be done
-                        and develop the task list to do it."
-              />
+              <div data-uk-scrollspy="cls: uk-animation-slide-left; delay: 400">
+                <Process
+                  image="static/process-1.svg"
+                  process="Project Planning"
+                  description="All the components needed to set up the project success
+                          will be described. It's the process you go through to
+                          establish the steps required to define our project
+                          objectives, clarify the scope of what needs to be done
+                          and develop the task list to do it."
+                />
+              </div>
             </Column>
             <Column width="1-2@s 1-1@l">
-              <Process
-                image="static/process-2.svg"
-                process="Review Existing Process"
-                description="Current workflow/process will be discussed with the Project Team in order to define what can be utilized, improved or integrated to enhance the process or feature of the system."
-              />
+              <div data-uk-scrollspy="cls: uk-animation-slide-right; delay: 500">
+                <Process
+                  image="static/process-2.svg"
+                  process="Review Existing Process"
+                  description="Current workflow/process will be discussed with the Project Team in order to define what can be utilized, improved or integrated to enhance the process or feature of the system."
+                />
+              </div>
             </Column>
             <Column width="1-2@s 1-1@l">
-              <Process
-                image="static/process-3.svg"
-                process="Training & Implementation"
-                description="This process includes the scheduling of trainees, training activities and the actual use of the software in a live environment. All end-users are primed and guided in the proper utilization of the software."
-              />
+              <div data-uk-scrollspy="cls: uk-animation-slide-left; delay: 600">
+                <Process
+                  image="static/process-3.svg"
+                  process="Training & Implementation"
+                  description="This process includes the scheduling of trainees, training activities and the actual use of the software in a live environment. All end-users are primed and guided in the proper utilization of the software."
+                />
+              </div>
             </Column>
             <Column width="1-2@s 1-1@l">
-              <Process
-                image="static/process-4.svg"
-                process="Post Implementation Support"
-                description="Rapid response times and coordination of efforts are key success factors. As part of the implementation plan, super users are usually required additional trainings on troubleshooting to boost internal support."
-              />
+              <div data-uk-scrollspy="cls: uk-animation-slide-right; delay: 700">
+                <Process
+                  image="static/process-4.svg"
+                  process="Post Implementation Support"
+                  description="Rapid response times and coordination of efforts are key success factors. As part of the implementation plan, super users are usually required additional trainings on troubleshooting to boost internal support."
+                />
+              </div>
             </Column>
           </Grid>
         </Column>
--- src/components/Index/Advantages.js
+++ src/components/Index/Advantages.js
@@ -50,7 +50,7 @@ const Advantages = () => {
           <p>Unbeatable advantages and value over any other provider</p>
         </Column>
         <Column>
-          <Grid childWidth="1-1 1-2@s 1-3@l">
+          <div className="uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@l" data-uk-grid="" data-uk-scrollspy="cls: uk-animation-scale-up; target: > div; delay:500; repeat: false">
             <Advantage
               icon="static/advantage-1.svg"
               advantage="Over 20 years of providing quality healthcare software"
@@ -99,13 +99,7 @@ const Advantages = () => {
               description="Lifetime Software Warranty enables you to use the software as long as you want and not worry of having to pay extra for maintenance or additional cost on support and after sales services."
               number="8"
             ></Advantage>
-            <Advantage
-              icon="static/advantage-9.svg"
-              advantage="Lifetime License and Maintenance"
-              description="You only pay once for your software and no need to pay a monthly or yearly maintenance fee. The longer you use your software the more it pays off."
-              number="9"
-            ></Advantage>
-          </Grid>
+          </div>
         </Column>
       </Grid>
     </Section>
