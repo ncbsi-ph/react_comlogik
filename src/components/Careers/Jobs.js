822e88edfdb30fd41019d9bad8aa54adafd38430
Ivy Gozarin <38131621+itsbunniie@users.noreply.github.com>
2020-05-20 16:05:04 +0800

    jobs modal

--- src/components/Careers/Jobs.js
+++ src/components/Careers/Jobs.js
@@ -69,15 +69,17 @@ const Jobs = () => {
         <Column>
           <Grid childWidth="1-1">
             <Column>
-              <div className="gray-bg uk-padding">
-                <Job
-                  title="HR Officer"
-                  openPositions="1"
-                  openUntil="May 30"
-                  workExperience="2-3"
-                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel purus id sapien condimentum rhoncus vel sit amet diam. Etiam dui augue, malesuada at lectus quis, scelerisque faucibus leo. In hac habitasse platea dictumst."
-                />
-              </div>
+              <a href="#job-details" data-uk-toggle="" type="button" className="job-modal">
+                <div className="gray-bg uk-padding">
+                  <Job
+                    title="HR Officer"
+                    openPositions="1"
+                    openUntil="May 30"
+                    workExperience="2-3"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel purus id sapien condimentum rhoncus vel sit amet diam. Etiam dui augue, malesuada at lectus quis, scelerisque faucibus leo. In hac habitasse platea dictumst."
+                  />
+                </div>
+              </a>
             </Column>
             <Column>
               <div className="gray-bg uk-padding">
@@ -93,6 +95,107 @@ const Jobs = () => {
           </Grid>
         </Column>
       </Grid>
+
+      <div id="job-details" class="uk-modal-container" data-uk-modal="">
+        <div class="uk-modal-dialog uk-modal-body">
+          <button class="uk-modal-close-default" type="button" data-uk-close=""></button>
+          <Grid childWidth="1-1">
+            <Column>
+              <h3 className="text-black">HR Officer</h3>
+            </Column>
+            <Column className="uk-margin-top">
+              <Grid childWidth="auto">
+                <Column className="uk-text-bold uk-flex uk-flex-middle uk-flex-center">
+                  <div className="uk-margin-small-right">
+                    <Icon icon={people} width={24} height={24}></Icon>
+                  </div>
+                  <div>
+                    1 open position
+                  </div>
+                </Column>
+                <Column className="uk-text-bold uk-flex uk-flex-middle">
+                  <div className="uk-margin-small-right">
+                    <Icon icon={calendar} width={24} height={24}></Icon>
+                  </div>
+                  <div>
+                    Open until May 30
+                  </div>
+                </Column>
+                <Column className="uk-text-bold uk-flex uk-flex-middle">
+                  <div className="uk-margin-small-right">
+                    <Icon icon={work} width={24} height={24}></Icon>
+                  </div>
+                  <div>
+                    Minimum of 2-3 years work related experience
+                  </div>
+                </Column>
+              </Grid>
+            </Column>
+            <Column>
+              <h5>Job Description</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis iaculis dui. In hac habitasse platea dictumst. Quisque lobortis lorem sit amet lorem sagittis, at placerat odio sollicitudin. Phasellus varius suscipit felis eget elementum. Aliquam eros risus, tempor nec odio nec, egestas vestibulum leo. Pellentesque tincidunt nulla eget nisi finibus, non congue quam vehicula. Nullam at quam lacinia, gravida tellus et, gravida lectus. Suspendisse commodo est id vulputate ultricies. Donec eget aliquet lacus. Morbi augue arcu, facilisis in est eget, dignissim malesuada turpis. Vivamus placerat justo ac leo aliquet blandit. Aliquam nunc nulla, malesuada dignissim justo a, faucibus volutpat eros. Maecenas quis eros nec felis pellentesque tristique. Proin et venenatis ipsum.</p>
+            </Column>
+            <Column>
+              <h5>Job Requirements</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis iaculis dui. In hac habitasse platea dictumst. Quisque lobortis lorem sit amet lorem sagittis, at placerat odio sollicitudin. Phasellus varius suscipit felis eget elementum. Aliquam eros risus, tempor nec odio nec, egestas vestibulum leo. Pellentesque tincidunt nulla eget nisi finibus, non congue quam vehicula. Nullam at quam lacinia, gravida tellus et, gravida lectus. Suspendisse commodo est id vulputate ultricies. Donec eget aliquet lacus. Morbi augue arcu, facilisis in est eget, dignissim malesuada turpis. Vivamus placerat justo ac leo aliquet blandit. Aliquam nunc nulla, malesuada dignissim justo a, faucibus volutpat eros. Maecenas quis eros nec felis pellentesque tristique. Proin et venenatis ipsum.</p>
+            </Column>
+            <Column>
+              <div className="gradient-bg-light uk-padding">
+                <h3>Apply for this position</h3>
+                <form>
+                  <div class="uk-grid-small" data-uk-grid="">
+                    <div class="uk-width-auto uk-flex uk-flex-right uk-flex-middle">
+                      <label class="uk-form-label" for="name">Full Name</label>
+                    </div>
+                    <div class="uk-width-expand">
+                      <input class="uk-input" type="text" id="name" />
+                    </div>
+                    <div class="uk-width-auto uk-flex uk-flex-right uk-flex-middle">
+                      <label class="uk-form-label" for="gender">Gender</label>
+                    </div>
+                    <div class="uk-width-1-5">
+                      <select class="uk-select" id="gender">
+                        <option>Male</option>
+                        <option>Female</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="uk-grid-small" data-uk-grid="">
+                    <div class="uk-width-auto uk-flex uk-flex-right uk-flex-middle">
+                      <label class="uk-form-label" for="email">Email Address</label>
+                    </div>
+                    <div class="uk-width-expand">
+                      <input class="uk-input" type="text" id="email" />
+                    </div>
+                    <div class="uk-width-auto uk-flex uk-flex-right uk-flex-middle">
+                      <label class="uk-form-label" for="mobile">Mobile Number</label>
+                    </div>
+                    <div class="uk-width-expand">
+                      <input class="uk-input" type="text" id="mobile" />
+                    </div>
+                  </div>
+                  <div class="uk-grid-small" data-uk-grid="">
+                    <div className="uk-width-expand">
+                      <input class="uk-input" type="text" placeholder="Upload your CV or Resume" disabled />
+                    </div>
+                  </div>
+                  <div class="uk-grid-small" data-uk-grid="">
+                    <div className="uk-width-expand">
+                      <label>
+                        <input class="uk-checkbox uk-margin-right" type="checkbox" checked />
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </label>
+                    </div>
+                  </div>
+                  <div className="uk-margin-top">
+                    <button className="uk-button uk-button-primary" type="submit">Submit your application</button>
+                  </div>
+                </form>
+              </div>
+            </Column>
+          </Grid>
+        </div>
+      </div>
     </Section>
   );
 };
