751a92e009b3142d3e267e357ef240824b67fbcf
Revo Jet Socorro <45466103+cysmix12@users.noreply.github.com>
2022-01-11 20:19:50 +0800

    Fixed modal images

--- src/components/FeaturedModal.js
+++ src/components/FeaturedModal.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import UIkit from 'uikit';
 
-import { Grid, Column } from '../components/Grid';
 import { getFeatured } from '../api';
 
 const FeaturedModal = () => {
@@ -30,45 +29,30 @@ const FeaturedModal = () => {
 
   return (
     <div ref={modalRef} data-uk-modal="">
-      <div className="uk-modal-dialog w-[750px] uk-margin-auto-vertical">
+      <div className="bg-transparent uk-modal-dialog uk-margin-auto-vertical">
         <button
           className="uk-modal-close-outside uk-close-large"
           type="button"
           data-uk-close=""
         />
-        <div className="uk-position-relative" tabIndex="-1" data-uk-slider="">
-          <ul
-            className="uk-slider-items uk-child-width-1-1 uk-visible-toggle uk-light"
-            data-uk-grid=""
-          >
-            {data.length ? (
-              data.map((featured) => (
-                <li key={featured.id}>
-                  <Grid childWidth="1-1" className="h-full">
-                    <Column className="flex items-center justify-center h-full">
-                      <img
-                        className="object-contain h-full"
-                        src={featured.image}
-                      />
-                    </Column>
-                  </Grid>
-                </li>
-              ))
-            ) : (
-              <li>
-                <div className="uk-flex uk-flex-center">
-                  <div data-uk-spinner="ratio: 3" />
-                </div>
+        <div data-uk-slider="">
+          <ul className="uk-slider-items">
+            {data.map((featured) => (
+              <li
+                className="flex items-center justify-center"
+                key={featured.id}
+              >
+                <img className="" src={featured.image} />
               </li>
-            )}
+            ))}
           </ul>
           <a
-            className="uk-position-center-left uk-hidden-hover uk-slidenav-large"
+            className="uk-position-center-left uk-slidenav-large"
             data-uk-slidenav-previous=""
             data-uk-slider-item="previous"
           />
           <a
-            className="uk-position-center-right uk-hidden-hover uk-slidenav-large"
+            className="uk-position-center-right uk-slidenav-large"
             data-uk-slidenav-next=""
             data-uk-slider-item="next"
           />
